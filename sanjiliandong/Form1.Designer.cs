9b9c8e11fec922c5345885f8ef7ead66526288e6
keith <sunzongll@163.com>
2019-12-02 15:19:22 +0800

    添加项目文件。

---
+++ sanjiliandong/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace sanjiliandong
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbsheng = new System.Windows.Forms.ListBox();
+            this.lbshi = new System.Windows.Forms.ListBox();
+            this.lbxian = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtb = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbsheng
+            // 
+            this.lbsheng.FormattingEnabled = true;
+            this.lbsheng.ItemHeight = 12;
+            this.lbsheng.Location = new System.Drawing.Point(6, 58);
+            this.lbsheng.Name = "lbsheng";
+            this.lbsheng.Size = new System.Drawing.Size(120, 64);
+            this.lbsheng.TabIndex = 0;
+            this.lbsheng.SelectedIndexChanged += new System.EventHandler(this.lbsheng_SelectedIndexChanged);
+            // 
+            // lbshi
+            // 
+            this.lbshi.FormattingEnabled = true;
+            this.lbshi.ItemHeight = 12;
+            this.lbshi.Location = new System.Drawing.Point(132, 58);
+            this.lbshi.Name = "lbshi";
+            this.lbshi.Size = new System.Drawing.Size(120, 64);
+            this.lbshi.TabIndex = 1;
+            this.lbshi.SelectedIndexChanged += new System.EventHandler(this.lbshi_SelectedIndexChanged);
+            // 
+            // lbxian
+            // 
+            this.lbxian.FormattingEnabled = true;
+            this.lbxian.ItemHeight = 12;
+            this.lbxian.Location = new System.Drawing.Point(258, 58);
+            this.lbxian.Name = "lbxian";
+            this.lbxian.Size = new System.Drawing.Size(120, 64);
+            this.lbxian.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.lbsheng);
+            this.groupBox1.Controls.Add(this.lbxian);
+            this.groupBox1.Controls.Add(this.lbshi);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(384, 142);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "产地";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(258, 38);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 12);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "县";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(132, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "市";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "省";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtb);
+            this.groupBox2.Controls.Add(this.btnAdd);
+            this.groupBox2.Location = new System.Drawing.Point(12, 161);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(384, 78);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "产地";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(303, 49);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "添加";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtb
+            // 
+            this.txtb.Location = new System.Drawing.Point(8, 36);
+            this.txtb.Name = "txtb";
+            this.txtb.Size = new System.Drawing.Size(244, 21);
+            this.txtb.TabIndex = 1;
+            this.txtb.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(407, 261);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbsheng;
+        private System.Windows.Forms.ListBox lbshi;
+        private System.Windows.Forms.ListBox lbxian;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtb;
+        private System.Windows.Forms.Button btnAdd;
+    }
+}
+
---
+++ sanjiliandong/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sanjiliandong
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lbsheng.Items.Add("山东省");
+            lbsheng.Items.Add("河北省");
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string address = "";
+            address += lbsheng.Text+" ";
+            address += lbshi.Text + " ";
+            address += lbxian.Text + " ";
+            txtb.Text = address;
+    }
+
+        private void lbsheng_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lbsheng.Text == "山东省")
+            {
+                lbshi.Items.Clear();
+                lbxian.Items.Clear();
+                lbshi.Items.Add("济南市");
+                lbshi.Items.Add("青岛市");
+                lbshi.Items.Add("烟台市");
+                
+            }
+            else if (lbsheng.Text =="河北省")
+            {
+                lbshi.Items.Clear();
+                lbxian.Items.Clear();
+                lbshi.Items.Add("石家庄");
+                lbshi.Items.Add("唐山市");
+                lbshi.Items.Add("廊坊市");
+            }
+        }
+
+        private void lbshi_SelectedIndexChanged(object sender, EventArgs e)
+        {
+             if (lbshi.Text == "济南市")
+             {
+                 lbxian.Items.Clear();
+                 lbxian.Items.Add("长清区");
+                 lbxian.Items.Add("历下区");
+                 lbxian.Items.Add("市中区");
+            }
+            else if (lbshi.Text == "青岛市")
+            {
+                lbxian.Items.Clear();
+                lbxian.Items.Add("市南区");
+                lbxian.Items.Add("市北区");
+                lbxian.Items.Add("黄岛区");
+            }
+            else if (lbshi.Text == "烟台市")
+            {
+                lbxian.Items.Clear();
+                lbxian.Items.Add("烟台市");
+                lbxian.Items.Add("烟台高新技术产业开发区");
+                lbxian.Items.Add("烟台经济技术开发区");
+            }
+            if (lbshi.Text == "石家庄")
+            {
+                lbxian.Items.Clear();
+                lbxian.Items.Add("长安区");
+                lbxian.Items.Add("桥西区");
+                lbxian.Items.Add("新华区");
+            }
+            else if (lbshi.Text == "唐山市")
+            {
+                lbxian.Items.Clear();
+                lbxian.Items.Add("路南区");
+                lbxian.Items.Add("路北区");
+                lbxian.Items.Add("古冶区");
+            }
+            else if (lbshi.Text == "廊坊市")
+            {
+                lbxian.Items.Clear();
+                lbxian.Items.Add("广阳区");
+                lbxian.Items.Add("安次区");
+                lbxian.Items.Add("霸州市");
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
